af3cdd777e6c96e25914bec31dc04d8947bcd3ff
Arthur <86860468+AortegaP30@users.noreply.github.com>
2023-06-16 22:40:57 -0400

    Add files via upload

--- Proyecto1/code/DivideAndConquer.kt
+++ Proyecto1/code/DivideAndConquer.kt
@@ -1,21 +1,21 @@
 
 
-fun divideAndConquerTSP(P: Array<Pair<Double, Double>>): Array<Array<Pair<Pair<Double, Double>, Pair<Double, Double>>>> {
+fun divideAndConquerTSP(P: Array<Pair<Double, Double>>): Array<Array<Pair<Double, Double>>> {
     val n = P.size
     if (n == 0) {
-        val tour = arrayOf(arrayOf(Pair(Pair(0.0,0.0),Pair(0.0,0.0))))
+        val tour = arrayOf(arrayOf(Pair(0.0,0.0),Pair(0.0,0.0)))
         return tour  
     }
     else if (n == 1) {
-        val tour = arrayOf(arrayOf(Pair(P[0],P[0]),Pair(P[0],P[0])))
+        val tour = arrayOf(arrayOf(P[0],P[0]))
         return tour
     }
     else if (n == 2){
-        val tour = arrayOf(arrayOf(Pair(P[0],P[1]),Pair(P[0],P[1])))
+        val tour = arrayOf(arrayOf(P[0],P[1]), arrayOf(P[0],P[1]))
         return tour
     }
     else if (n == 3){
-        val tour = arrayOf(arrayOf(Pair(P[0],P[1]),Pair(P[0],P[3]),Pair(P[1],P[3])))
+        val tour =  arrayOf(arrayOf(P[0],P[1]), arrayOf(P[0],P[1]), arrayOf(P[1],P[2]))
         return tour
     }
     else {
