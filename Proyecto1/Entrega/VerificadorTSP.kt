bc825f4ea00d8eec26586dcf81fbe99fe79f0f1d
bau <18-10665@usb.ve>
2023-06-18 21:49:38 -0400

    Crear carpeta entrega

---
+++ Proyecto1/Entrega/VerificadorTSP.kt
@@ -0,0 +1,112 @@
+import java.io.File
+import kotlin.system.exitProcess
+
+
+/**
+ * Prints an error message and exits the program.
+ *
+ * @param message The error message to print.
+ */
+fun errorMessage(message: String) {
+    println("Error: $message")
+    exitProcess(1)
+}
+
+/**
+ * Verifica que todas las ciudades estén en el tour.
+ * 
+ * @param ciudades Arreglo de ciudades.
+ * @param tour Arreglo de tours.
+ */
+fun verificarCiudades(ciudades: Array<Pair<Double, Double>>, tour: Array<Array<Pair<Double, Double>>>) {
+    for (i in 0 until ciudades.size) {
+        val elemento = ciudades[i]
+        for (j in 0 until tour.size) {
+            val contiene = elemento in tour[i]
+            if (!contiene) {
+                errorMessage("Falta una ciudad en el arreglo")
+            }
+        }
+    }
+}
+
+
+/**
+ * Verifica que el tamaño del tour sea el correcto.
+ * 
+ * @param ciudades Arreglo de ciudades.
+ * @param tour Arreglo de tours.
+
+ */
+fun verificarTamano(ciudades: Array<Pair<Double, Double>>, tour: Array<Array<Pair<Double, Double>>>) {
+    if (ciudades.size != tour.size/2) {
+        errorMessage("Hay duplicados o hay recorridos repetidos en el tour")
+    }
+}
+
+/**
+ * Abre un archivo de texto y lo convierte en un arreglo de ciudades.
+ * 
+ * @param archivo Nombre del archivo.
+ * @return Arreglo de ciudades.
+ */
+fun abrirCiudades(archivo: String): Array<Pair<Double, Double>> {
+    val lineas:Array<String> = File(archivo).readLines().toTypedArray()
+    var ciudades = Array<Pair<Double, Double>>(0) { Pair(0.0, 0.0) }
+
+    for (linea in lineas) {
+        val lineSplit = linea.split("\\s".toRegex()).toTypedArray()
+        try {
+            lineSplit[0].toInt()        // Verifica si es un número
+        } catch (e: NumberFormatException) {
+            continue
+        }
+        val x = lineSplit[1].toDouble()
+        val y = lineSplit[2].toDouble()
+        ciudades += Pair(x, y)          // Agrega la ciudad a la lista
+    }
+    return ciudades
+}
+
+/** 
+ * Abre un archivo de texto solucion y lo convierte en un arreglo de tours.
+ * 
+ * @param archivo Nombre del archivo.
+ * @return Arreglo de tours.
+ */
+fun abrirSolucion(archivo: String): Array<Int> {
+    val lineas:Array<String> = File(archivo).readLines().toTypedArray()
+    var ciudades = Array<Int>(0) {0}
+
+    for (linea in lineas) {
+        val lineSplit = linea.split("\\s".toRegex()).toTypedArray()
+        try {
+            val num = lineSplit[0].toInt()        // Verifica si es un número
+            ciudades += num
+        } catch (e: NumberFormatException) {
+            continue
+        }
+
+    }
+    return ciudades
+}
+
+fun main(args: Array<String>) {
+    val instancia = args[0]
+    val solucion = args[1]
+
+
+    val ciudades = abrirCiudades(instancia)
+    val tour = abrirSolucion(solucion)
+
+    println(ciudades.size)
+    println(ciudades.joinToString(", "))
+    println(tour.size)
+    println(tour.joinToString(", "))
+
+
+    // verificarCiudades(ciudades, tour)
+    // verificarTamano(ciudades, tour)
+
+    println("todo correcto")
+}
---
+++ Proyecto1/Entrega/runVerificadorTSP.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+kotlin VerificadorTSPKt.class $*
