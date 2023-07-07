4541a6737716557e2fd649ba345e5b3f34ab28a4
Arthur <86860468+AortegaP30@users.noreply.github.com>
2023-07-07 17:05:40 -0400

    Add files via upload

--- Laboratorio Sem 9/code/CodigoMorse.kt
+++ Laboratorio Sem 9/code/CodigoMorse.kt
@@ -1,29 +1,33 @@
-class CodigoMorse() {
+class CodigoMorse {
+    private val arbolMorse: Nodo
 
-    fun decodificarLetra(secuencia: String): String? {
-        
-        for (caracter in secuencia) {
-
+    init {
+        arbolMorse = Nodo()
+        val letras = arrayOf("E", "T", "I", "A", "N", "M", "S", "U", "R", "W", "D", "K", "G", "O", "H", "V", "F", "", "L", "", "P", "J", "B", "X", "C", "Y", "Z", "Q")
+        val codigos = arrayOf(".", "-", "..", ".-", "-.", "--", "...", "..-", ".-.", ".--", "-..", "-.-", "--.", "---",
+            "....", "...-", "..-.", "", ".-..", "", ".--.", ".---",
+            "-...", "-..-", "-.-.", "-.--",
+            "--..","--.-")
+        for (i in letras.indices) {
+            insertar(arbolMorse, codigos[i], letras[i])
         }
-
-        return null
     }
 
-    fun decodificarMensaje(frase: Array<String>): String {
-        
-        var mensaje = ""
-
-        for (letras in frase) {
-            if (letras == "/") {
-                mensaje += " "
-            } else {
-                val letra = decodificarLetra(letras)
-                mensaje += letra
+    private fun insertar(nodo: Nodo, codigo: String, letra: String) {
+        if (codigo.isEmpty()) {
+            nodo.letra = letra
+            return
+        }
+        if (codigo[0] == '.') {
+            if (nodo.izquierda == null) {
+                nodo.izquierda = Nodo()
             }
+            insertar(nodo.izquierda!!, codigo.substring(1), letra)
+        } else {
+            if (nodo.derecha == null) {
+                nodo.derecha = Nodo()
+            }
+            insertar(nodo.derecha!!, codigo.substring(1), letra)
         }
-    
-        return mensaje
     }
-
-
 }
