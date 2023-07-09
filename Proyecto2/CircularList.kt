2919f408307e047b1e22db61fcb380c52e2b6798
bau <18-10665@usb.ve>
2023-07-09 16:13:43 -0400

    hash palabra

--- Proyecto2/CircularList.kt
+++ Proyecto2/CircularList.kt
@@ -1,23 +1,23 @@
 class CircularList {
 
-    var cabeza: HashTableEntry? = null
+    var cabeza: Palabra? = null
 
-    fun agregarLista(clave: HashTableEntry) {
+    fun agregarLista(clave: Palabra) {
         clave.siguiente = cabeza
         cabeza?.anterior = clave
-        cabeza = clave  
+        cabeza = clave
     }
 
-    fun eliminarLista(clave: HashTableEntry) {
+    fun eliminarLista(clave: Palabra) {
         if (clave.anterior == null) cabeza = clave.siguiente
             
         clave.anterior?.siguiente = clave.siguiente
         clave.siguiente?.anterior = clave.anterior
     }
 
-    fun buscarLista(clave: String): HashTableEntry? {
-        var c: HashTableEntry? = cabeza
-        while (c != null && c.clave != clave) c = c.siguiente
+    fun buscarLista(clave: String): Palabra? {
+        var c: Palabra? = cabeza
+        while (c != null && c.palabra != clave) c = c.siguiente
         return c
     }
 }
